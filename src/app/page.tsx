2740752cef039eb154c2c979c12b052e1ce0dfb5
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-03-17 07:47:27 +0800

    button onClick fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { addWarehouse } from "@/actions/add-warehouse";
 
 export default function Home() {
-  const handleClick = () : void => {
+  const handleClick = () => {
     try {
       addWarehouse();
     } catch (err) {
@@ -12,7 +12,7 @@ export default function Home() {
   };
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <button onClick={handleClick()}>Try Me!</button>
+      <button onClick={()=>handleClick()}>Try Me!</button>
     </div>
   );
 }
